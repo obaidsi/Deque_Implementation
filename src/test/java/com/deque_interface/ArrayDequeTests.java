da89de0ecd479ea0a64fc65af4d7cf97afa16797
obaidsi <52484343+obaidsi@users.noreply.github.com>
2023-09-13 12:07:28 -0700

    Add test for ArrayDeque

---
+++ src/test/java/com/deque_interface/ArrayDequeTests.java
@@ -0,0 +1,15 @@
+package com.deque_interface;
+/**
+ * Tests for the {@link ArrayDeque} class.
+ *
+ * @see ArrayDeque
+ */
+public class ArrayDequeTests extends DequeTests {
+    @Override
+    public <T> Deque<T> createDeque() {
+        return new ArrayDeque<>();
+    }
+
+    // You may write additional tests here if you only want them to run for ArrayDeque
+
+}
